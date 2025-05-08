697d8c605a4cb2c63a47301870b812fb4e1ce1b3
Youssef <ezzatyousser79@gmail.com>
2025-05-08 19:29:52 +0300

    Fix Navbar path casing for Netlify build

--- src/Pages/LayoutPage.jsx
+++ src/Pages/LayoutPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Outlet } from "react-router";
-import Navbar from "../components/Navbar";
-import Footer from "../components/Footer";
+import Navbar from "../Components/Navbar";
+import Footer from "../Components/Footer";
 import Classes from "../Styles/Layout.module.css"
 
 
